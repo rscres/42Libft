adfdb07a7f8bf70bca626a513d740e77d8080290
Renato Seelaender crescenti <rseelaen@c3r7p3.42sp.org.br>
2023-04-25 20:46:48 -0300

    added malloc to memmove, added const to strlen param

--- src/mem/ft_memmove.c
+++ src/mem/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 14:29:24 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/04/25 18:20:01 by rseelaen         ###   ########.fr       */
+/*   Updated: 2023/04/25 20:45:37 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	char		*buf_dest;
 	const char	*buf_src;
 
+	buf_dest = malloc(ft_strlen(dest) * sizeof(int));
 	i = 0;
 	buf_dest = dest;
 	buf_src = src;
--- src/str/ft_strlen.c
+++ src/str/ft_strlen.c
@@ -6,13 +6,13 @@
 /*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 12:09:06 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/04/25 20:11:00 by rseelaen         ###   ########.fr       */
+/*   Updated: 2023/04/25 20:45:49 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strlen(char *str)
+int	ft_strlen(const char *str)
 {
 	int	length;
 
