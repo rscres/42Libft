ac685ee7df7f84fe1f65360a372d5c4236f35737
rscres <renatocrescenti@gmail.com>
2023-04-26 18:46:43 -0300

    added ft_substr .c file and removed ft_strlen declaration from ft_strdup

--- src/str/ft_substr.c
+++ src/str/ft_substr.c
@@ -1,31 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: renato <renato@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/18 15:33:26 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/04/26 18:03:04 by renato           ###   ########.fr       */
+/*   Created: 2023/04/26 17:49:27 by renato            #+#    #+#             */
+/*   Updated: 2023/04/26 18:42:40 by renato           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(char *src)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	size_t	i;
+	size_t	j;
 	char	*dest;
-	int		len;
-	int		i;
 
-	i = 0;
-	len = ft_strlen(src);
-	dest = malloc((len + 1) * sizeof(char));
-	while (src[i])
+	dest = malloc(len * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	i = (size_t)start;
+	j = 0;
+	while (j < len)
 	{
-		dest[i] = src[i];
+		*(dest + j) = *(s + i);
 		i++;
+		j++;
 	}
-	dest[i] = '\0';
 	return (dest);
 }
