68b97d1cd78b822f6429778ee2113620e12ad1b3
rscres <renatocrescenti@gmail.com>
2023-04-26 20:03:11 -0300

    moved ft_strjoin to /src/str and added null termination to ft_substr

--- src/str/ft_strjoin.c
+++ src/str/ft_strjoin.c
@@ -1,34 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: renato <renato@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/26 17:49:27 by renato            #+#    #+#             */
-/*   Updated: 2023/04/26 18:54:40 by renato           ###   ########.fr       */
+/*   Created: 2023/04/26 18:49:46 by renato            #+#    #+#             */
+/*   Updated: 2023/04/26 19:53:08 by renato           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
+	char	*str;
+	size_t	len1;
+	size_t	len2;
 	size_t	i;
-	size_t	j;
-	char	*dest;
 
-	dest = malloc(len * sizeof(char));
-	if (dest == NULL)
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	str = malloc((len1 + len2) * sizeof(char));
+	if (str == NULL)
 		return (NULL);
-	i = (size_t)start;
-	j = 0;
-	while (j < len)
-	{
-		*(dest + j) = *(s + i);
-		i++;
-		j++;
-	}
-	*(dest + j) = '\0';
-	return (dest);
+	i = 0;
+	while (i++ < len1)
+		*(str + i - 1) = *(s1 + i - 1);
+	i = 0;
+	while (i++ < len2)
+		*(str + len1 + i - 1) = *(s2 + i - 1);
+	*(str + len1 + i) = '\0';
+	return (str);
 }
