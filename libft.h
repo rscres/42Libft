4bdf3404a629ee2c272653dfa9c6fc776ddb6216
rscres <renatocrescenti@gmail.com>
2023-04-26 18:48:23 -0300

    added ft_calloc, ft_strrchr and ft_substr prototypes to libft.h

--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
+/*   By: renato <renato@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 15:53:03 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/04/25 17:40:16 by rseelaen         ###   ########.fr       */
+/*   Updated: 2023/04/26 18:10:06 by renato           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,10 @@ char	*ft_strcpy(char *dest, const char *src);
 char	*ft_strncpy(char *dest, const char *src, unsigned int n);
 char	*ft_strdup(const char *src);
 char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
 char	*ft_strstr(const char *str, const char *to_find);
 char	*ft_strnstr(const char *str, const char *to_find, size_t len);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_strlen(const char *str);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, unsigned int n);
@@ -48,7 +50,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *ptr, int c, size_t num);
-void	*ft_memalloc(size_t size);
+void	*ft_calloc(size_t nitems, size_t size);
 void	ft_bzero(void *s, size_t n);
 
 // Convertion functions
