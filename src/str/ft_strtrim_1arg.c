54f61cfd478c42b6cf3aef6094e4ee82897642e3
rscres <renatocrescenti@gmail.com>
2023-05-01 23:28:20 -0300

    modified ft_strtrim from 1 to 2 params(added set to be trimmed), renamed old as ft_strtrim_1arg

--- src/str/ft_strtrim_1arg.c
+++ src/str/ft_strtrim_1arg.c
@@ -6,32 +6,29 @@
 /*   By: renato <renato@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 23:50:46 by renato            #+#    #+#             */
-/*   Updated: 2023/05/01 23:24:18 by renato           ###   ########.fr       */
+/*   Updated: 2023/04/28 15:50:17 by renato           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s)
 {
 	int		start;
 	int		len;
 	int		i;
-	int		j;
 	char	*trim;
 
-	len = ft_strlen(s1);
+	len = ft_strlen(s);
 	start = 0;
 	i = 0;
-	j = ft_strlen(set);
-	while (s1[len] == set[j--])
+	while (s[len] == ' ' || s[len] == '\t' || s[len] == '\n')
 		len--;
-	j = 0;
-	while (s1[start] == set[j++])
+	while (s[start] == ' ' || s[start] == '\n' || s[start] == '\t')
 		start++;
 	trim = malloc((len - start) * sizeof(char));
-	while (start <= len)
-		trim[i++] = s1[start++];
+	while (start < len - 1)
+		trim[i++] = s[start++];
 	trim[i] = '\0';
 	return (trim);
 }
