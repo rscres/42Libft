ca12f0936699a4896400165ebd446237b2d7c29a
Renato Seelaender crescenti <rseelaen@c3r6p3.42sp.org.br>
2023-05-02 19:33:11 -0300

    fixed strlcpy: added strlen return for size 0

---
+++ ft_strlcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 20:01:35 by rseelaen          #+#    #+#             */
+/*   Updated: 2023/05/02 19:31:28 by rseelaen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+unsigned int	ft_strlcpy(char *dest, const char *src, unsigned int size)
+{
+	unsigned int		i;
+
+	i = 0;
+	if (size == 0)
+		return (strlen(src));
+	if (size < 0)
+		size = 1;
+	while (*(src + i) && i < size - 1)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	while (*(src + i))
+		i++;
+	return (i);
+}
