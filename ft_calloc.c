8d16d2e412877fb68488d7f03bfbe075353f1e71
rscres <rseelaen@student.42.org.br>
2023-05-03 17:24:53 -0300

    fixed calloc: malloc size changed to nmemb * size and added SIZE_MAX condition

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,21 @@
 /*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 14:29:21 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/04/27 11:58:46 by rseelaen         ###   ########.fr       */
+/*   Updated: 2023/05/03 17:15:38 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nitems, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*alloc;
 
-	alloc = malloc(nitems * size);
+	if (nmemb == SIZE_MAX || size == SIZE_MAX)
+		return (NULL);
+	alloc = malloc(nmemb * size);
 	if (alloc == NULL)
 		return (NULL);
-	ft_bzero(alloc, sizeof(alloc));
+	ft_bzero(alloc, nmemb * size);
 	return (alloc);
 }
