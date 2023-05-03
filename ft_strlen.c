e1b19d426c417b4e98287894bbdb40f10c4c6eae
rscres <rseelaen@student.42.org.br>
2023-05-03 13:35:57 -0300

    strlen changed int -> size_t and changed prototype to same as man

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,18 +6,18 @@
 /*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 12:09:06 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/04/25 20:45:49 by rseelaen         ###   ########.fr       */
+/*   Updated: 2023/05/03 13:34:13 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strlen(const char *str)
+size_t	ft_strlen(const char *s)
 {
-	int	length;
+	size_t	length;
 
 	length = 0;
-	while (*str++)
+	while (*s++)
 		length++;
 	return (length);
 }
