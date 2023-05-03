3a960399678d01898b280ce1c85ad7e9f9a02588
rscres <rseelaen@student.42.org.br>
2023-05-03 17:48:21 -0300

    strmapi: added NULL conditions

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 21:11:10 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/05/02 13:56:31 by rseelaen         ###   ########.fr       */
+/*   Updated: 2023/05/03 17:46:51 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,17 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char				*newstr;
 	unsigned int		i;
 
+	if (!s || !f)
+		return (NULL);
 	i = 0;
-	newstr = malloc(ft_strlen(s) * sizeof(char));
+	newstr = malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (!newstr)
+		return (NULL);
 	while (s[i])
 	{
 		newstr[i] = f(i, s[i]);
 		i++;
 	}
+	newstr[i] = '\0';
 	return (newstr);
 }
