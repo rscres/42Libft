e60a2878893de9a124db4c6ebd7f06230b48a73a
rscres <rseelaen@student.42.org.br>
2023-05-03 12:19:09 -0300

    fixed strjoin: passing all tests

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 18:49:46 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/04/27 11:58:47 by rseelaen         ###   ########.fr       */
+/*   Updated: 2023/05/03 12:17:23 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*str;
 	size_t	len1;
 	size_t	len2;
-	size_t	i;
+	int		i;
 
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
-	str = malloc((len1 + len2) * sizeof(char));
+	str = malloc((len1 + len2 + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	i = 0;
-	while (i++ < len1)
-		*(str + i - 1) = *(s1 + i - 1);
-	i = 0;
-	while (i++ < len2)
-		*(str + len1 + i - 1) = *(s2 + i - 1);
-	*(str + len1 + i) = '\0';
+	i = -1;
+	while (++i <= (int)len1)
+		*(str + i) = *(s1 + i);
+	i -= 1;
+	while (i++ < (int)(len2 + len1))
+		*(str + i - 1) = *(s2 + i - len1 - 1);
+	*(str + i - 1) = '\0';
 	return (str);
 }
