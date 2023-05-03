fdeeaf1cfea1bd57f0182015f29a6de3dd117511
rscres <rseelaen@student.42.org.br>
2023-05-03 14:18:27 -0300

    fixed strnstr: changed loop condition from i < len to i + little len <= len

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: rseelaen <rseelaen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 15:01:21 by rseelaen          #+#    #+#             */
-/*   Updated: 2023/05/02 21:23:53 by rseelaen         ###   ########.fr       */
+/*   Updated: 2023/05/03 14:11:06 by rseelaen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,18 @@ static int	cmp(const char *s1, const char *s2)
 	return (1);
 }
 
-char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t		i;
-	char	*temp;
+	char		*temp;
 
 	i = 0;
-	temp = (char *)str;
-	
-	if (*to_find == '\0')
+	temp = (char *)big;
+	if (*little == '\0')
 		return (temp);
-	while (str[i] != '\0' && i < len)
+	while (big[i] != '\0' && i + ft_strlen(little) <= len)
 	{
-		if ((str[i] == to_find[0]) && cmp(&str[i], to_find))
+		if ((big[i] == little[0]) && cmp(&big[i], little))
 			return (&temp[i]);
 		i++;
 	}
